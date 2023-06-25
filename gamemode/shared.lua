0e6555d9bc8d7c42df108a4b443ac5046374fce9
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-06-25 21:19:29 +0530

    Create shared.lua

---
+++ gamemode/shared.lua
@@ -0,0 +1,16 @@
+--shared.lua
+
+GM.Name = "paintball" --game mode name
+GM.Author = "spaciouscoder78"--author
+GM.Email = "n/a"--email
+GM.Website = "n/a"--website
+
+--deriving game mode
+DeriveGamemode("sandbox")
+
+
+--initializing
+function GM:Initialize()
+    --do stuff
+
+end
