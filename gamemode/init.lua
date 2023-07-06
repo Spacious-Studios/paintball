a4c9081fe6b00d835396c5944fa51f78e9479c07
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-07-06 14:46:04 +0530

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -9,12 +9,20 @@ include("shared.lua")
 
 
 
-
+--playerspawn event
 function GM:PlayerSpawn(ply)
     ply:SetMaxHealth(100)
     ply:SetRunSpeed(300)
     ply:Give("paintball_gun_blue _model1")
     ply:SetupHands()
+
+
+end
+
+--player death event
+function GM:PlayerDeath(victim, inflictor,attacker)
+    --incrementing kill count value
+    inflictor:AddFrags(1)
 
 
 end
